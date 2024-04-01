9aa2cdad8adffddd663f0defca5836669ef07c60
Anton Seliverstov <antoncadetsil@gmail.com>
2024-04-01 23:28:33 +0100

    Init project setup

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,22 +1,43 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.ArrayList;
 
 public class MainWindow {
     private JButton buttonDecipher;
     private JButton buttonCipher;
-    private JTextPane textBefore;
-    private JTextPane textAfter;
+    private JScrollPane textBeforeScroll;
     public JPanel mainPanel;
     private JLabel cipherKey;
     private JButton buttonFileOpen;
+    private JScrollPane textAfterScroll;
+    private JTextPane textBefore;
+    private JTextPane textAfter;
 
     public MainWindow() {
+
         buttonFileOpen.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                //Main.gui.setPanel(new fileOpenWindow().openFilePanel);
-                int fileChooserVal = new fileOpenWindow().fileChooser.showOpenDialog(mainPanel);
+                fileOpenWindow fileOpenWindow = new fileOpenWindow();
+                int fileChooserVal = fileOpenWindow.fileChooser.showOpenDialog(mainPanel);
+                if (fileChooserVal == JFileChooser.APPROVE_OPTION) {
+                    File file = fileOpenWindow.fileChooser.getSelectedFile();
+                    try {
+                        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+                        ArrayList<String> arrayList = new ArrayList<>();
+                        while (bufferedReader.ready()) {
+                            arrayList.add(bufferedReader.readLine());
+                        }
+                        textBefore.setText(arrayList.toString());
+                    } catch (FileNotFoundException ex) {
+                        throw new RuntimeException(ex);
+                    } catch (IOException ex) {
+                        throw new RuntimeException(ex);
+                    }
+
+                }
             }
         });
     }
