f1a4ec6016ca7360b6eae4a47a41f60e0b4db764
Anton Seliverstov <antoncadetsil@gmail.com>
2024-04-03 16:24:34 +0100

    Initial project setup

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -5,40 +5,32 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class MainWindow {
-    private JButton buttonDecipher;
-    private JButton buttonCipher;
-    private JScrollPane textBeforeScroll;
     public JPanel mainPanel;
-    private JLabel cipherKey;
-    private JButton buttonFileOpen;
-    private JScrollPane textAfterScroll;
-    private JTextPane textBefore;
-    private JTextPane textAfter;
-    private JFileChooser fileChooser;
+    public JButton buttonDecipher, buttonCipher, buttonFileOpen;
+    public JScrollPane textBeforeScroll, textAfterScroll;
+    public JLabel cipherKey;
+    public JTextPane textBefore, textAfter;
+    public JFileChooser fileChooser;
 
     public MainWindow() {
-
-        buttonFileOpen.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                fileChooser = new JFileChooser();
-                int fileChooserVal = fileChooser.showOpenDialog(mainPanel);
-                if (fileChooserVal == JFileChooser.APPROVE_OPTION) {
-                    File file = fileChooser.getSelectedFile();
-                    try {
-                        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
-                        ArrayList<String> arrayList = new ArrayList<>();
-                        while (bufferedReader.ready()) {
-                            arrayList.add(bufferedReader.readLine());
-                        }
-                        textBefore.setText(arrayList.toString());
-                    } catch (FileNotFoundException ex) {
-                        throw new RuntimeException(ex);
-                    } catch (IOException ex) {
-                        throw new RuntimeException(ex);
+        buttonFileOpen.addActionListener(e -> {
+            fileChooser = new JFileChooser();
+            int fileChooserVal = fileChooser.showOpenDialog(mainPanel);
+            if (fileChooserVal == JFileChooser.APPROVE_OPTION) {
+                File file = fileChooser.getSelectedFile();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+                    ArrayList<String> arrayList = new ArrayList<>();
+                    while (bufferedReader.ready()) {
+                        arrayList.add(bufferedReader.readLine());
                     }
-
+                    textBefore.setText(arrayList.toString());
+                } catch (FileNotFoundException ex) {
+                    throw new RuntimeException(ex);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
                 }
+
             }
         });
     }
